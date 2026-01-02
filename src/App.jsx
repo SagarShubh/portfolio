e83dbfcb8029241c0b17ad1e37c08313192d8927
SagarShubh <41685423+SagarShubh@users.noreply.github.com>
2026-01-02 15:17:22 +0530

    Fix: App.jsx Black Screen Crash (Restored Imports)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Lenis from 'lenis';
@@ -13,6 +13,32 @@ import { HelmetProvider } from 'react-helmet-async';
 
 import Legal from './pages/Legal';
 import NotFound from './pages/NotFound';
+
+// Scroll to top on route change
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/project/:id" element={<ProjectPage />} />
+        <Route path="/legal" element={<Legal />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
 
 function App() {
@@ -32,21 +58,29 @@ function App() {
     setLenis(lenisInstance)
 
     function raf(time) {
+      lenisInstance.raf(time)
+      requestAnimationFrame(raf)
     }
 
-    requestAnimationFrame(raf);
-  }, []);
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenisInstance.destroy()
+    }
+  }, [])
 
   return (
     <HelmetProvider>
-      <Router>
+      <div className="App bg-black text-white min-h-screen cursor-none">
+        <Preloader />
         <CustomCursor />
-        <ScrollToTop />
-        {/* Cinematic Noise Overlay Global */}
-        <div className="fixed inset-0 z-[9999] pointer-events-none opacity-[0.03] mix-blend-overlay bg-[url('https://grainy-gradients.vercel.app/noise.svg')]"></div>
+        <GrainOverlay />
 
-        <AnimatedRoutes />
-      </Router>
+        <Router>
+          <ScrollToTop />
+          <AnimatedRoutes />
+        </Router>
+      </div>
     </HelmetProvider>
   );
 }
